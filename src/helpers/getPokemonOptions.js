cea9f1d0e24b812bff8f7213a2fee05e096522a6
Alangobe <alangobe01@gmail.com>
2022-03-24 17:08:23 -0500

    Axios y nombres de 4 pokemons

--- src/helpers/getPokemonOptions.js
+++ src/helpers/getPokemonOptions.js
@@ -1,3 +1,4 @@
+import pokemonApi from "@/api/pokemonApi"
 
 const getPokemons = () => {
     const pokemonsArr = Array.from( Array(650) )
@@ -6,11 +7,25 @@ const getPokemons = () => {
 
 const getPokemonOptions = () => {
     const mixedPokemons = getPokemons().sort( () => Math.random() - 0.5)
-    getPokemonNames(mixedPokemons.splice(0,4))
+    const pokemons = await getPokemonNames(mixedPokemons.splice(0,4))
+    console.table(pokemons)
 }
 
-const getPokemonNames = ([pok1, pok2, pok3, pok4] = []) => {
-    console.log(pok1, pok2, pok3, pok4)
+const getPokemonNames = async ([pok1, pok2, pok3, pok4] = []) => {
+    const arrPromises=[
+        pokemonApi.get(`/${a}`),
+        pokemonApi.get(`/${b}`),
+        pokemonApi.get(`/${c}`),
+        pokemonApi.get(`/${d}`),
+    ]
+    const [p1, p2, p3, p4] = await Promise.all(arrPromises)
+    
+    return[
+        {name: p1.data.name, id: p1.data.id},
+        {name: p2.data.name, id: p2.data.id},
+        {name: p3.data.name, id: p3.data.id},
+        {name: p4.data.name, id: p4.data.id}
+    ]
 }
 
 export default getPokemonOptions
